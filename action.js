4fc20dfb436cb07221035df62041dbc1e4fbeb21
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-01-26 17:20:42 -0500

    Update
    Got the json file to show up, now is just a matter of stylizing the page and making it responsive.

--- action.js
+++ action.js
@@ -1,6 +1,11 @@
-fetch("./data.json")
+const listE1 = document.getElementById("resultSection");
+fetch('./data.json')
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
+        data.forEach(post => {
+            // icon | category | Score
+            listE1.insertAdjacentHTML('beforeend', `<img src="${post.icon}" class="icon" alt="icon"></img>` + `<p class="category">${post.category}</p>` + `<p class="score">${post.score}/100 </p>`);
+        });
     })
     .catch((error) => console.error("error loading json file", error));
