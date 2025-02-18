cccae404a014a3c74dee5f093a84dc8f667a3fca
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-02-17 22:31:16 -0500

    Update - mobile
    Continued stylizing, figured out how to manipulate the json data a bit further appending them to the html...

--- action.js
+++ action.js
@@ -1,11 +1,13 @@
-const listE1 = document.getElementById("resultSection");
+const listE1 = document.getElementById("score-category");
 fetch('./data.json')
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
         data.forEach(post => {
             // icon | category | Score
-            listE1.insertAdjacentHTML('beforeend', `<img src="${post.icon}" class="icon" alt="icon"></img>` + `<p class="category">${post.category}</p>` + `<p class="score"><strong>${post.score}</strong> / 100 </p>`);
+            listE1.insertAdjacentHTML ('beforeend', 
+                `<div class="category"><img src="${post.icon}" class="icon" alt="icon"></img> <p class="category-name">${post.category}</p><p class="score"><strong>${post.score}</strong> / 100 </p></div>`
+            );
         });
     })
     .catch((error) => console.error("error loading json file", error));
