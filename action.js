76b76891f20d1c18468ca2b944d89bb5d1ea5498
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-02-19 22:11:07 -0500

    Update - mobile vp
    Stylized the mobile viewport, will adjust and work on desktop viewport later.

--- action.js
+++ action.js
@@ -6,7 +6,15 @@ fetch('./data.json')
         data.forEach(post => {
             // icon | category | Score
             listE1.insertAdjacentHTML ('beforeend', 
-                `<div class="category"><img src="${post.icon}" class="icon" alt="icon"></img> <p class="category-name">${post.category}</p><p class="score"><strong>${post.score}</strong> / 100 </p></div>`
+                `<div class="category" id="${post.category}">
+                    <div class="left-side">
+                        <img src="${post.icon}" class="icon" alt="icon"></img> 
+                        <p class="category-name">${post.category}</p>
+                    </div>
+                    <div class="right-side">
+                        <p class="score"><strong>${post.score}</strong> / 100 </p>
+                    </div>
+                </div>`
             );
         });
     })
